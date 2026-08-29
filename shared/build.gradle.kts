47d5178402d1e8f2f55e25f9ee5b7ef85cff4228
cavidyrm <javid.yrm@gmail.com>
2026-08-29 23:17:28 +0330

    fixed deploy issue

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,26 +1,25 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidMultiplatformLibrary)
+    // alias(libs.plugins.androidMultiplatformLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
-    listOf(
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "Shared"
-            isStatic = true
-        }
-    }
+    // listOf(
+    //     iosArm64(),
+    //     iosSimulatorArm64()
+    // ).forEach { iosTarget ->
+    //     iosTarget.binaries.framework {
+    //         baseName = "Shared"
+    //         isStatic = true
+    //     }
+    // }
     
-    jvm()
+    // jvm()
     
     js {
         browser()
@@ -31,34 +30,34 @@ kotlin {
         browser()
     }
     
-    android {
-       namespace = "com.vitran.shop.shared"
-       compileSdk = libs.versions.android.compileSdk.get().toInt()
-       minSdk = libs.versions.android.minSdk.get().toInt()
-    
-       compilerOptions {
-           jvmTarget = JvmTarget.JVM_11
-       }
-       androidResources {
-           enable = true
-       }
-       withHostTest {
-           isIncludeAndroidResources = true
-       }
-       withDeviceTestBuilder {
-           sourceSetTreeName = "test"
-       }.configure {
-           instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-       }
-    }
+    // android {
+    //    namespace = "com.vitran.shop.shared"
+    //    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    //    minSdk = libs.versions.android.minSdk.get().toInt()
+    //
+    //    compilerOptions {
+    //        jvmTarget = JvmTarget.JVM_11
+    //    }
+    //    androidResources {
+    //        enable = true
+    //    }
+    //    withHostTest {
+    //        isIncludeAndroidResources = true
+    //    }
+    //    withDeviceTestBuilder {
+    //        sourceSetTreeName = "test"
+    //    }.configure {
+    //        instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    //    }
+    // }
     
     sourceSets {
-        androidMain.dependencies {
-            implementation(libs.compose.uiToolingPreview)
-            implementation(libs.compose.uiTooling)
-            implementation(libs.ktor.client.android)
-            implementation(libs.media3.exoplayer)
-        }
+        // androidMain.dependencies {
+        //     implementation(libs.compose.uiToolingPreview)
+        //     implementation(libs.compose.uiTooling)
+        //     implementation(libs.ktor.client.android)
+        //     implementation(libs.media3.exoplayer)
+        // }
         commonMain.dependencies {
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
@@ -76,12 +75,12 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
-        iosMain.dependencies {
-            implementation(libs.ktor.client.darwin)
-        }
-        jvmMain.dependencies {
-            implementation(libs.ktor.client.java)
-        }
+        // iosMain.dependencies {
+        //     implementation(libs.ktor.client.darwin)
+        // }
+        // jvmMain.dependencies {
+        //     implementation(libs.ktor.client.java)
+        // }
         jsMain.dependencies {
             implementation(libs.wrappers.browser)
         }
@@ -91,6 +90,6 @@ kotlin {
     }
 }
 
-dependencies {
-    androidRuntimeClasspath(libs.compose.uiTooling)
-}
+// dependencies {
+//     androidRuntimeClasspath(libs.compose.uiTooling)
+// }
